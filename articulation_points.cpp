eb89f5bd7d0269e4452f5dd08a35e17f13fba021
keeganasmith <keeganasmith2003@gmail.com>
2025-02-24 14:15:00 -0600

    always add edges connected to known first

--- articulation_points.cpp
+++ articulation_points.cpp
@@ -1,14 +1,14 @@
 #include "uf.h"
 #include "articulation_points.h"
-using std::unordered_set, std::min, std::max, std::set, std::unordered_map, std::vector, std::pair, std::cout;
-vector<pair<int, int>> construct_largest_neighbor_edges(vector<pair<int, int>>& edges){
+using std::sort, std::unordered_set, std::min, std::max, std::queue, std::set, std::unordered_map, std::vector, std::pair, std::cout;
+vector<Edge_With_Count> construct_largest_neighbor_edges(vector<pair<int, int>>& edges){
   unordered_map<int, int> vertex_degree_mapping;
   for(int i =0 ; i < edges.size(); i++){
     vertex_degree_mapping[edges.at(i).first]++;
     vertex_degree_mapping[edges.at(i).second]++;
   }
   
-  unordered_map<int, pair<int, int>> largest_neighbor_mapping;
+  unordered_map<int, vector<pair<int, int>>> largest_neighbor_mapping;
   for(int i = 0; i < edges.size(); i++){
     int vertex_one = edges.at(i).first;
     int vertex_two = edges.at(i).second;
@@ -18,29 +18,43 @@ vector<pair<int, int>> construct_largest_neighbor_edges(vector<pair<int, int>>& 
     int vertex_one_curr_largest_neighbor_size = 0;
     int vertex_two_curr_largest_neighbor_size = 0;
     if(largest_neighbor_mapping.count(vertex_one) != 0){
-      vertex_one_curr_largest_neighbor_size = largest_neighbor_mapping[vertex_one].second;
+      vertex_one_curr_largest_neighbor_size = largest_neighbor_mapping[vertex_one].back().second;
     }
     if(largest_neighbor_mapping.count(vertex_two) != 0){
-      vertex_two_curr_largest_neighbor_size = largest_neighbor_mapping[vertex_two].second;
+      vertex_two_curr_largest_neighbor_size = largest_neighbor_mapping[vertex_two].back().second;
     }
     if(vertex_one_size > vertex_two_curr_largest_neighbor_size){
-      largest_neighbor_mapping[vertex_two] = pair<int, int>(vertex_one, vertex_one_size);
+      largest_neighbor_mapping[vertex_two] = vector<pair<int, int>>();
+      largest_neighbor_mapping[vertex_two].push_back(pair<int, int>(vertex_one, vertex_one_size));
+    }
+    else if(vertex_one_size == vertex_two_curr_largest_neighbor_size){
+      largest_neighbor_mapping[vertex_two].push_back(pair<int, int>(vertex_one, vertex_one_size));
     }
     if(vertex_two_size > vertex_one_curr_largest_neighbor_size){
-      largest_neighbor_mapping[vertex_one] = pair<int, int>(vertex_two, vertex_two_size);
+      largest_neighbor_mapping[vertex_one] = vector<pair<int, int>>();
+      largest_neighbor_mapping[vertex_one].push_back(pair<int, int>(vertex_two, vertex_two_size));
+    }
+    else if(vertex_two_size == vertex_one_curr_largest_neighbor_size){
+      largest_neighbor_mapping[vertex_one].push_back(pair<int, int>(vertex_two, vertex_two_size));
     }
   }
-  vector<pair<int, int>> result;
+  vector<Edge_With_Count> result;
   set<pair<int, int>> already_accounted;
   for(auto it = largest_neighbor_mapping.begin(); it != largest_neighbor_mapping.end(); it++){
-    int start_vertex = min(it->first, it->second.first);
-    int end_vertex = max(it->first, it->second.first);
-    pair<int, int> edge_pair(start_vertex, end_vertex);
-    if(already_accounted.count(edge_pair) != 0){
-      continue;
+    for(int i = 0; i < it->second.size(); i++){
+      int start_vertex = min(it->first, it->second.at(i).first);
+      int end_vertex = max(it->first, it->second.at(i).first);
+      Edge_With_Count curr_edge;
+      curr_edge.vertex_one = start_vertex;
+      curr_edge.vertex_two = end_vertex;
+      curr_edge.count = 0;
+      pair<int, int> edge_pair(start_vertex, end_vertex);
+      if(already_accounted.count(edge_pair) != 0){
+        continue;
+      }
+      result.push_back(curr_edge);
+      already_accounted.emplace(edge_pair);
     }
-    result.push_back(edge_pair);
-    already_accounted.emplace(edge_pair);
   }
   return result;
 }
@@ -54,7 +68,7 @@ vector<int> articulation_points(vector<pair<int, int>>& edges, size_t num_vertic
   vector<int> result;
   set<pair<int, int>> known_edges;
   vector<pair<int, int>> unknown_edges(edges);
-  vector<pair<int, int>> largest_neighbor_edges = construct_largest_neighbor_edges(edges);
+  vector<Edge_With_Count> largest_neighbor_edges = construct_largest_neighbor_edges(edges);
   unordered_set<int> known_vertices;
   while(true){
     Union_Find uf(num_vertices);
@@ -66,21 +80,41 @@ vector<int> articulation_points(vector<pair<int, int>>& edges, size_t num_vertic
         vertex_degree_spanning[it->second]++;
       }
     }
-
+    
+    vector<pair<int, int>> edges_connecting_two_unknowns;
     for(int i = 0; i < largest_neighbor_edges.size(); i++){
-      bool merged = uf.merge(largest_neighbor_edges.at(i).first, largest_neighbor_edges.at(i).second);
+      int vertex_one = largest_neighbor_edges.at(i).vertex_one;
+      int vertex_two = largest_neighbor_edges.at(i).vertex_two;
+      if(known_vertices.count(vertex_one) == 0 && known_vertices.count(vertex_two) == 0){
+        edges_connecting_two_unknowns.push_back(pair<int, int>(vertex_one, vertex_two));
+        continue;
+      }
+      bool merged = uf.merge(vertex_one, vertex_two);
       if(merged){
-        vertex_degree_spanning[largest_neighbor_edges.at(i).first]++;
-        vertex_degree_spanning[largest_neighbor_edges.at(i).second]++;
+        vertex_degree_spanning[vertex_one]++;
+        vertex_degree_spanning[vertex_two]++;
+        largest_neighbor_edges.at(i).count++;
       }
     }
     for(int i = 0; i < unknown_edges.size(); i++){
+      if(known_vertices.count(unknown_edges.at(i).first) == 0 && known_vertices.count(unknown_edges.at(i).second) == 0){
+        edges_connecting_two_unknowns.push_back(unknown_edges.at(i));
+        continue;
+      }
       bool merged = uf.merge(unknown_edges.at(i).first, unknown_edges.at(i).second);
       if(merged){
         vertex_degree_spanning[unknown_edges.at(i).first]++;
         vertex_degree_spanning[unknown_edges.at(i).second]++;
       }
     }  
+
+    for(int i = 0;i < edges_connecting_two_unknowns.size(); i++){
+      bool merged = uf.merge(edges_connecting_two_unknowns.at(i).first, edges_connecting_two_unknowns.at(i).second);
+      if(merged){
+        vertex_degree_spanning[edges_connecting_two_unknowns.at(i).first]++;
+        vertex_degree_spanning[edges_connecting_two_unknowns.at(i).second]++;
+      }
+    }
     bool found_new_vertex = false;
     //find vertices which have degree <= 1
     int new_vertices_found = 0;
@@ -91,7 +125,6 @@ vector<int> articulation_points(vector<pair<int, int>>& edges, size_t num_vertic
         new_vertices_found++;
       }
     }
-    cout << "found " << new_vertices_found << " new vertices\n";
     if(!found_new_vertex){
       break;
     }
@@ -104,6 +137,12 @@ vector<int> articulation_points(vector<pair<int, int>>& edges, size_t num_vertic
         known_edges.emplace(pair<int, int>(first_vertex, second_vertex));
       }
     }
+
+    sort(largest_neighbor_edges.begin(), largest_neighbor_edges.end(),
+          [](const Edge_With_Count& a, const Edge_With_Count& b) {
+              return a.count < b.count;
+          });
+
   }
   
   unordered_set<int> already_placed;
--- articulation_points.h
+++ articulation_points.h
@@ -1,3 +1,8 @@
 #include <bits/stdc++.h>
 using std::vector, std::pair;
+struct Edge_With_Count{
+  int vertex_one;
+  int vertex_two;
+  int count;
+};
 vector<int> articulation_points(vector<pair<int, int>>& edges, size_t num_vertices);
