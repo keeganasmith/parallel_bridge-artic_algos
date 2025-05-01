52d837bb168eb2b22fa37eef6bf4a448e9a101ed
Keegan Smith <keeganasmith2003@gmail.com>
2025-05-01 16:41:02 -0500

    more debug statements

--- bridges.cpp
+++ bridges.cpp
@@ -204,6 +204,7 @@ void label_propagation(ygm::container::set<pair<long long, long long>>& edges, y
   static ygm::container::map<long long, long long>* s_ccids;
   static ygm::container::map<long long, long long>* s_parents;
   static ygm::container::bag<pair<long long, long long>>* s_spanning_tree;
+  static ygm::comm* s_world = &world;
   s_edges = &edges;
   s_ccids = &ccids;
   s_parents = &parents;
@@ -219,13 +220,13 @@ void label_propagation(ygm::container::set<pair<long long, long long>>& edges, y
           long long v_ccid = value;
           //edge is u, v
           if(u_ccid < v_ccid){
-            world.cout0("got here");
+            s_world->cout0("got here");
             s_ccids->async_insert_or_assign(edge.second, u_ccid);
             s_parents->async_insert_or_assign(edge.second, edge.first);
             local_updated = true;
           } 
           else if(v_ccid < u_ccid){
-            world.cout("got here");
+            s_world->cout("got here");
             s_ccids->async_insert_or_assign(edge.first, v_ccid);
             s_parents->async_insert_or_assign(edge.first, edge.second);
             local_updated = true;
