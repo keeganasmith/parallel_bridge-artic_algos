3fe446979ea0f162fb79392ef63595c4cb8ac94d
Keegan Smith <keeganasmith2003@gmail.com>
2025-05-01 21:21:00 -0500

    debug statements

--- bridges.cpp
+++ bridges.cpp
@@ -229,6 +229,9 @@ void label_propagation(ygm::container::set<pair<long long, long long>>& edges, y
   s_sign = sign;
   auto apply_sign_function = [](const pair<long long, long long>& edge){
     s_ccids->async_visit(edge.first, [](const long long& vertex, const long long& value){
+      if(s_sign < 0){
+        s_world->cout0("value: ", value);
+      }
       if(s_sign < 0 && value > 0){
         s_world->cout0("got here");
         s_ccids->local_insert_or_assign(vertex, value * s_sign);
